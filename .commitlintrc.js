0314bd1b412bd8037baa39f93d6ce936892a0d22
解园园 <xieyuanyuan@unovo.com.cn>
2018-06-05 14:33:39 +0800

    fix: test

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"], rules:{}
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test']
+    ]
+  }
 }
