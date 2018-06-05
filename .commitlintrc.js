d116a781048c8db239bfc60db86ec487f2af5d90
解园园 <xieyuanyuan@unovo.com.cn>
2018-06-05 14:48:46 +0800

    feat: 修改commit配置

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,20 +1,4 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'ci',
-        'chore',
-        'docs',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test']
-    ]
-  }
+  rules: {}
 }
